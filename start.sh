166284099abe2fadb38bc375b725b5c1f680b746
Ben <ben@notionparallax.co.uk>
2023-08-09 15:01:03 +0000

    fix paths

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # 🛑🛑🛑 STOP! Don't run this yet! 🛑🛑🛑
 
-pip install -r ../course/requirements.txt
-bash ../course/extensions.sh
+pip install -r ./requirements.txt
+bash ./extensions.sh
 
 # Run this file by copying this command to the terminal:
 # bash codespace_startup.sh
